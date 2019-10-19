66dd4a431c429b7a33f87d41ce296fbb207a80f8
AlexandrKoliukh <alexkoliukh@gmial.com>
2019-10-19 10:58:29 +0300

    Validating

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import rssInputValidator from './rssInputValidation';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+document.addEventListener('DOMContentLoaded',() => {
+  const a = document.getElementById('rssUrl');
+  a.addEventListener('input', rssInputValidator);
+});
